codelabs-ch/muen-tools

commit 6495fe216b029533567777deee763cc86a602956

Mutools and Muxml: Add 'subject_devicetree' type
Extend libmutools and libmuxml with a 'subject_
devicetree' type to supported physical memory
regions in policy files to be able to extract
Linux VM subjects and dtb memory.

libmutools/src/mutools-types.ads
@@ -64,7 +64,7 @@ is
       Subject_Channel, Subject_State, Subject_Timed_Event, Subject_Interrupts,
       Subject_Scheduling_Info, Subject_Bios, Subject_Acpi_Rsdp,
       Subject_Acpi_Xsdt, Subject_Acpi_Fadt, Subject_Acpi_Dsdt, Subject_Device,
-      Subject_Solo5_Boot_Info, Subject_Crash_Audit,
+      Subject_Solo5_Boot_Info, Subject_Crash_Audit, Subject_Devicetree,
       Device_Rmrr);
 
    --  Memory reserved for system use. Can neither be referenced by kernel nor

libmuxml/schema/memory.xsd
@@ -78,6 +78,7 @@
    <xs:enumeration value="subject_scheduling_info"/>
    <xs:enumeration value="subject_timed_event"/>
    <xs:enumeration value="subject_interrupts"/>
+   <xs:enumeration value="subject_devicetree"/>
    <xs:enumeration value="device_rmrr"/>
   </xs:restriction>
  </xs:simpleType>

libmuxml/schema/system_src/memory.xsd
@@ -257,6 +257,7 @@
    <xs:enumeration value="subject_solo5_boot_info"/>
    <xs:enumeration value="subject_device"/>
    <xs:enumeration value="subject_timed_event"/>
+   <xs:enumeration value="subject_devicetree"/>
   </xs:restriction>
  </xs:simpleType>
 

commit 7fb7b20f651c5f16b5ff4c251fe72953f5c1eecf

Fix addition of subject_devicetree
Fixes: 6495fe216b02 ("Mutools and Muxml: Add 'subject_devicetree' type")

libmutools/data/cyclic_group_references.xsd
@@ -853,6 +853,7 @@
    <xs:enumeration value="subject_scheduling_info"/>
    <xs:enumeration value="subject_timed_event"/>
    <xs:enumeration value="subject_interrupts"/>
+   <xs:enumeration value="subject_devicetree"/>
    <xs:enumeration value="device_rmrr"/>
   </xs:restriction>
  </xs:simpleType><xs:complexType name="memoryBlockBaseType" abstract="true">
@@ -1947,6 +1948,7 @@
    <xs:enumeration value="subject_solo5_boot_info"/>
    <xs:enumeration value="subject_device"/>
    <xs:enumeration value="subject_timed_event"/>
+   <xs:enumeration value="subject_devicetree"/>
   </xs:restriction>
  </xs:simpleType><xs:complexType name="majorFrameType">
   <xs:annotation>

libmutools/data/default_schema.xsd
@@ -370,6 +370,7 @@
       <xs:enumeration value="subject_scheduling_info"/>
       <xs:enumeration value="subject_timed_event"/>
       <xs:enumeration value="subject_interrupts"/>
+      <xs:enumeration value="subject_devicetree"/>
       <xs:enumeration value="device_rmrr"/>
     </xs:restriction>
   </xs:simpleType>
@@ -845,6 +846,7 @@
       <xs:enumeration value="subject_solo5_boot_info"/>
       <xs:enumeration value="subject_device"/>
       <xs:enumeration value="subject_timed_event"/>
+      <xs:enumeration value="subject_devicetree"/>
     </xs:restriction>
   </xs:simpleType>
   <xs:complexType name="majorFrameType">

libmutools/src/mutools-types.ads
@@ -75,7 +75,7 @@ is
    subtype Kernel_Memory is Memory_Kind range Kernel .. Kernel_Vmcs;
 
    --  Memory mappable by subjects.
-   subtype Subject_Memory is Memory_Kind range Subject .. Subject_Crash_Audit;
+   subtype Subject_Memory is Memory_Kind range Subject .. Subject_Devicetree;
 
    --  Memory usable for device domains/DMA.
    subtype DMA_Memory is

mugensinfo/src/sinfo-utils.adb
@@ -49,7 +49,8 @@ is
            MTT.Subject_Acpi_Dsdt       => Musinfo.Subject_Acpi_Dsdt,
            MTT.Subject_Device          => Musinfo.Subject_Device,
            MTT.Subject_Solo5_Boot_Info => Musinfo.Subject_Solo5_Boot_Info,
-           MTT.Subject_Crash_Audit     => Musinfo.Subject_Crash_Audit);
+           MTT.Subject_Crash_Audit     => Musinfo.Subject_Crash_Audit,
+           MTT.Subject_Devicetree      => Musinfo.Subject_Devicetree);
 
    --  Convert given subject memory to Musinfo representation. Allowed values
    --  are Mutools.Types.Subject_Memory and Mutools.Types.Kernel_Interface