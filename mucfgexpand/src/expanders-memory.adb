3ef5097cb61fb10b1c195ce304c052821c880f07
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2024-07-12 12:53:58 +0200

    Mucfgexpand: Use 3 levels for ARMv8 subject PTs
    When calculating the size of ARMv8 subject page tables, use a 3-level
    layout since on ARMv8 Stage2 uses 3 paging levels.

--- mucfgexpand/src/expanders-memory.adb
+++ mucfgexpand/src/expanders-memory.adb
@@ -29,6 +29,7 @@ with Mulog;
 with Muxml.Utils;
 with Mutools.Utils;
 with Mutools.Constants;
+with Mutools.System_Config;
 with Mutools.XML_Utils;
 with Mucfgcheck.Kernel;
 with Mucfgcheck.Subject;
@@ -615,6 +616,15 @@ is
         := McKae.XML.XPath.XIA.XPath_Query
           (N     => Data.Doc,
            XPath => "/system/hardware/devices/device");
+      Is_ARM_System : constant Boolean := Mutools.System_Config.Has_Boolean
+        (Data => Data,
+         Name => "armv8") and then
+        Mutools.System_Config.Get_Value
+          (Data => Data,
+           Name => "armv8");
+      PT_Levels : constant Natural
+        := (if Is_ARM_System then 3
+            else 4);
    begin
 
       --  Validate that there are no overlapping subject memory mappings and
@@ -636,7 +646,7 @@ is
             Size      : constant Interfaces.Unsigned_64
               := XML_Utils.Calculate_PT_Size
                 (Policy             => Data,
-                 Paging_Levels      => 4,
+                 Paging_Levels      => PT_Levels,
                  Large_Pages        => False,
                  Physical_Memory    => Physical_Mem,
                  Physical_Devices   => Physical_Devs,
