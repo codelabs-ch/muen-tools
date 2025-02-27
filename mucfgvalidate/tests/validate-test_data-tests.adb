12b25831db0efe738c64f02a3f1391e0e86c3b98
Reto Buerki <reet@codelabs.ch>
2025-02-27 17:35:34 +0100

    Mucfgvalidate: Register both stack addr checks (x86)

--- mucfgvalidate/tests/validate-test_data-tests.adb
+++ mucfgvalidate/tests/validate-test_data-tests.adb
@@ -74,7 +74,7 @@ package body Validate.Test_Data.Tests is
                    File => "data/test_policy.xml");
 
       Register_X86_64;
-      Assert (Condition => XML_Processors.Get_Count = 155,
+      Assert (Condition => XML_Processors.Get_Count = 156,
               Message   => "Count mismatch:"
               & XML_Processors.Get_Count'Img);
       XML_Processors.Clear;
