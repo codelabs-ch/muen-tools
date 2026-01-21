77a18a2496db2e1b25d883f6628307c54956b09f
Reto Buerki <reet@codelabs.ch>
2026-01-21 15:33:26 +0100

    Auto-reformat linux-gencspec.py with black

--- scripts/linux-gencspec.py
+++ scripts/linux-gencspec.py
@@ -7,7 +7,7 @@ import sys
 
 import muutils
 
-DESCRIPTION = 'Linux XML component spec generator'
+DESCRIPTION = "Linux XML component spec generator"
 LINUX_VIRTUAL_ADDRESS = "16#0040_0000#"
 INITRAMFS_VIRTUAL_ADDRESS = 0x90000000
 
@@ -16,8 +16,9 @@ BOOT_HEAP_SIZE = 0x10000
 BOOT_STACK_SIZE = 0x4000
 
 
-def add_provides_memory(xml_spec, name, region_type, address, filename, size,
-                        executable, writable):
+def add_provides_memory(
+    xml_spec, name, region_type, address, filename, size, executable, writable
+):
     """
     Add file-backed memory region with given name and size to <provides>
     section of XML spec. The size is rounded up to the next 4K.
@@ -25,19 +26,26 @@ def add_provides_memory(xml_spec, name, region_type, address, filename, size,
     provides = xml_spec.xpath("/component/provides")[0]
     mem_size = size
     mem_size -= mem_size % -4096
-    print("* Adding '" + name + "' region with size " + str(mem_size)
-          + " bytes, address " + address + ", type " + region_type)
-    mem = etree.Element("memory",
-                        logical=name,
-                        virtualAddress=address,
-                        size=muutils.int_to_ada_hex(mem_size),
-                        executable=executable,
-                        writable=writable,
-                        type=region_type)
-    etree.SubElement(mem,
-                     "file",
-                     filename=filename,
-                     offset="none")
+    print(
+        "* Adding '"
+        + name
+        + "' region with size "
+        + str(mem_size)
+        + " bytes, address "
+        + address
+        + ", type "
+        + region_type
+    )
+    mem = etree.Element(
+        "memory",
+        logical=name,
+        virtualAddress=address,
+        size=muutils.int_to_ada_hex(mem_size),
+        executable=executable,
+        writable=writable,
+        type=region_type,
+    )
+    etree.SubElement(mem, "file", filename=filename, offset="none")
     provides.append(mem)
 
 
@@ -47,8 +55,7 @@ def get_initramfs_attrs(xml_spec):
     existing initramfs. Also return whether the initramfs mapping is executable
     and writable.
     """
-    phys_regions = xml_spec.xpath("/system/memory/memory"
-                                  + "[@type='subject_initrd']")
+    phys_regions = xml_spec.xpath("/system/memory/memory" + "[@type='subject_initrd']")
     if len(phys_regions) > 1:
         print("Warning: Muen system policy has multiple initramfs regions.")
         return None
@@ -56,16 +63,17 @@ def get_initramfs_attrs(xml_spec):
     if len(phys_regions) == 0:
         return muutils.int_to_ada_hex(INITRAMFS_VIRTUAL_ADDRESS), False, False
 
-    phys_name = phys_regions[0].attrib['name'].lower()
-    mems = xml_spec.xpath("/system/subjects/subject/memory/memory[@physical='"
-                          + phys_name + "']")
+    phys_name = phys_regions[0].attrib["name"].lower()
+    mems = xml_spec.xpath(
+        "/system/subjects/subject/memory/memory[@physical='" + phys_name + "']"
+    )
     if len(mems) == 0:
         return muutils.int_to_ada_hex(INITRAMFS_VIRTUAL_ADDRESS), False, False
 
-    size = muutils.ada_hex_to_int(phys_regions[0].attrib['size'])
-    virtual_address = mems[0].attrib['virtualAddress']
-    executable = mems[0].attrib['executable']
-    writable = mems[0].attrib['writable']
+    size = muutils.ada_hex_to_int(phys_regions[0].attrib["size"])
+    virtual_address = mems[0].attrib["virtualAddress"]
+    executable = mems[0].attrib["executable"]
+    writable = mems[0].attrib["writable"]
 
     """
     Check that all existing initramfs mappings are located at the same virtual
@@ -75,11 +83,11 @@ def get_initramfs_attrs(xml_spec):
     Also, only set executable/writable to true if all mappings allow it.
     """
     for mapping in mems:
-        if mapping.attrib['virtualAddress'] != virtual_address:
+        if mapping.attrib["virtualAddress"] != virtual_address:
             print("Warning: Initramfs mappings not at same virtual address.")
             return None, False, False
-        executable = executable and mapping.attrib['executable']
-        writable = writable and mapping.attrib['writable']
+        executable = executable and mapping.attrib["executable"]
+        writable = writable and mapping.attrib["writable"]
 
     new_address = muutils.ada_hex_to_int(virtual_address) + size
     return muutils.int_to_ada_hex(new_address), executable, writable
@@ -90,16 +98,15 @@ def parse_args():
     Returned parsed command line arguments
     """
     arg_parser = argparse.ArgumentParser(description=DESCRIPTION)
-    arg_parser.add_argument('kernel_binary', type=str,
-                            help='Linux kernel binary')
-    arg_parser.add_argument('src_xml_spec', type=str,
-                            help='Muen component source XML specification')
-    arg_parser.add_argument('--out_spec', type=str,
-                            help=('Filename of generated Muen component XML'))
-    arg_parser.add_argument('--initramfs', type=str,
-                            help='Linux Modules Initramfs')
-    arg_parser.add_argument('--src_policy', type=str,
-                            help=('Muen XML system policy'))
+    arg_parser.add_argument("kernel_binary", type=str, help="Linux kernel binary")
+    arg_parser.add_argument(
+        "src_xml_spec", type=str, help="Muen component source XML specification"
+    )
+    arg_parser.add_argument(
+        "--out_spec", type=str, help=("Filename of generated Muen component XML")
+    )
+    arg_parser.add_argument("--initramfs", type=str, help="Linux Modules Initramfs")
+    arg_parser.add_argument("--src_policy", type=str, help=("Muen XML system policy"))
 
     return arg_parser.parse_args()
 
@@ -125,13 +132,20 @@ if src_initramfs_path is not None:
         sys.exit("Error: Muen source system policy XML not specified")
 
     if not os.path.isfile(src_policy_path):
-        sys.exit("Error: Muen source system policy XML not found '"
-                 + src_policy_path + "'")
+        sys.exit(
+            "Error: Muen source system policy XML not found '" + src_policy_path + "'"
+        )
 
 out_spec_dir = os.path.dirname(out_spec_path)
 if len(out_spec_dir) > 0 and not os.path.isdir(out_spec_dir):
-    sys.exit(("Error: Output directory for component specification does not "
-              + "exist ('" + os.path.dirname(out_spec_dir) + "')"))
+    sys.exit(
+        (
+            "Error: Output directory for component specification does not "
+            + "exist ('"
+            + os.path.dirname(out_spec_dir)
+            + "')"
+        )
+    )
 
 print("Reading source component specification from '" + src_spec_path + "'")
 src_spec_name = os.path.basename(src_spec_path)
@@ -142,14 +156,16 @@ print("Processing Linux binary '" + src_bin_path + "'")
 binary_name = os.path.basename(src_bin_path)
 binary_size = os.path.getsize(src_bin_path)
 binary_size += BOOT_HEAP_SIZE + BOOT_STACK_SIZE
-add_provides_memory(src_spec,
-                    "binary",
-                    "subject_binary",
-                    LINUX_VIRTUAL_ADDRESS,
-                    binary_name,
-                    binary_size,
-                    "true",
-                    "true")
+add_provides_memory(
+    src_spec,
+    "binary",
+    "subject_binary",
+    LINUX_VIRTUAL_ADDRESS,
+    binary_name,
+    binary_size,
+    "true",
+    "true",
+)
 
 if src_initramfs_path is not None:
     print("Reading source system policy from '" + src_policy_path + "'")
@@ -162,16 +178,18 @@ if src_initramfs_path is not None:
         print("Processing initramfs '" + src_initramfs_path + "'")
         initramfs_name = os.path.basename(src_initramfs_path)
         initramfs_size = os.path.getsize(src_initramfs_path)
-        add_provides_memory(src_spec,
-                            "modules_initramfs",
-                            "subject_initrd",
-                            initramfs_addr,
-                            initramfs_name,
-                            initramfs_size,
-                            executable,
-                            writable)
+        add_provides_memory(
+            src_spec,
+            "modules_initramfs",
+            "subject_initrd",
+            initramfs_addr,
+            initramfs_name,
+            initramfs_size,
+            executable,
+            writable,
+        )
 
 
-with open(out_spec_path, 'wb') as out_spec:
+with open(out_spec_path, "wb") as out_spec:
     print("Writing component specification to '" + out_spec_path + "'")
     out_spec.write(etree.tostring(src_spec, pretty_print=True))
