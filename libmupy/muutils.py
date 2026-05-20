0a07425f4a465f128ebe78f17eba89c56f44c77d
Reto Buerki <reet@codelabs.ch>
2026-05-20 18:08:15 +0200

    muutils.py: Fix int_to_ada_hex(0)
    str.lstrip("0x") does NOT strip the prefix '0x' but any character 0|x.
    Which results in empty string for input 0 -> 0x0.

--- libmupy/muutils.py
+++ libmupy/muutils.py
@@ -27,7 +27,7 @@ def int_to_ada_hex(value):
     if value == "":
         return ""
     else:
-        val = hex(value).rstrip("L").lstrip("0x")
+        val = hex(value).rstrip("L")[2:]
         val = val.rjust(int(math.ceil(len(val) / 4.0) * 4), "0")
         hexvalue = "16#"
         for index in range(0, len(val)):
